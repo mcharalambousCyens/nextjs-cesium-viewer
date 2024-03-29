19017c03d3c5cc7b6abb232d367e35fa9fea5588
mchara40 <55321012+mchara40@users.noreply.github.com>
2024-03-29 12:17:54 +0200

    dfsdf

--- next.config.js
+++ next.config.js
@@ -1,69 +1,79 @@
-// next.config.js
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+// // next.config.js
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const path = require('path');
 
    
-module.exports = {
+// module.exports = {
   
-  reactStrictMode: false,
-  webpack: (config, { isServer, webpack }) => {
-    output:"export";
-    // Exclude Cesium workers from TerserPlugin
-    if (!isServer) {
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            // {
-            //   from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Workers"),
-            //   to: "public/Cesium/Workers"
-            // },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/ThirdParty"),
-              to: "public/Cesium/ThirdParty"
-            },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Assets"),
-              to: "public/Cesium/Assets"
-            },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Widgets"),
-              to: "public/Cesium/Widgets"
-            }
-          ]
-        }),
-        new webpack.DefinePlugin({
-          CESIUM_BASE_URL: JSON.stringify("/Cesium")
-        })
-      );
+//   reactStrictMode: false,
+//   webpack: (config, { isServer, webpack }) => {
+//     output:"export";
+//     // Exclude Cesium workers from TerserPlugin
+//     if (!isServer) {
+//       config.plugins.push(
+//         new CopyWebpackPlugin({
+//           patterns: [
+//             // {
+//             //   from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Workers"),
+//             //   to: "public/Cesium/Workers"
+//             // },
+//             {
+//               from: path.join(__dirname, "node_modules/cesium/Build/Cesium/ThirdParty"),
+//               to: "public/Cesium/ThirdParty"
+//             },
+//             {
+//               from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Assets"),
+//               to: "public/Cesium/Assets"
+//             },
+//             {
+//               from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Widgets"),
+//               to: "public/Cesium/Widgets"
+//             }
+//           ]
+//         }),
+//         new webpack.DefinePlugin({
+//           CESIUM_BASE_URL: JSON.stringify("/Cesium")
+//         })
+//       );
 
-      // Optional: Explicitly modify the TerserPlugin options if needed
-      config.optimization.minimizer = config.optimization.minimizer.map(plugin => {
-        if (plugin.constructor.name === 'TerserPlugin') {
-          plugin.options.terserOptions = {
-            ...plugin.options.terserOptions,
-            // Prevent Terser from parsing the worker files
-            mangle: false,
-            compress: false,
-            parse: {
-              ecma: 8
-            },
-            module: true,
-          };
-        }
-        return plugin;
-      });
-    }
+//       // Optional: Explicitly modify the TerserPlugin options if needed
+//       config.optimization.minimizer = config.optimization.minimizer.map(plugin => {
+//         if (plugin.constructor.name === 'TerserPlugin') {
+//           plugin.options.terserOptions = {
+//             ...plugin.options.terserOptions,
+//             // Prevent Terser from parsing the worker files
+//             mangle: false,
+//             compress: false,
+//             parse: {
+//               ecma: 8
+//             },
+//             module: true,
+//           };
+//         }
+//         return plugin;
+//       });
+//     }
 
-    return config;
-  },
-};
-//@ts-check
+//     return config;
+//   },
+// };
+// //@ts-check
  
+// // @ts-check
+ 
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   out
+// // };
+ 
+// // module.exports = nextConfig;
+
 // @ts-check
  
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   out
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ output:'export',
+ distDir:'dir'
+};
  
-// module.exports = nextConfig;
+module.exports = nextConfig;
