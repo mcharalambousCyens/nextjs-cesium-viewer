ed478e3e5300e2ba8bebdce902ba8cad06e502d5
mchara40 <55321012+mchara40@users.noreply.github.com>
2024-03-29 11:11:58 +0200

    ffsfd

--- next.config.js
+++ next.config.js
@@ -1,57 +1,8 @@
-// next.config.js
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
-
-module.exports = {
-  
-  reactStrictMode: false,
-  webpack: (config, { isServer, webpack }) => {
-    // Exclude Cesium workers from TerserPlugin
-    if (!isServer) {
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            // {
-            //   from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Workers"),
-            //   to: "public/Cesium/Workers"
-            // },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/ThirdParty"),
-              to: "public/Cesium/ThirdParty"
-            },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Assets"),
-              to: "public/Cesium/Assets"
-            },
-            {
-              from: path.join(__dirname, "node_modules/cesium/Build/Cesium/Widgets"),
-              to: "public/Cesium/Widgets"
-            }
-          ]
-        }),
-        new webpack.DefinePlugin({
-          CESIUM_BASE_URL: JSON.stringify("/Cesium")
-        })
-      );
-
-      // Optional: Explicitly modify the TerserPlugin options if needed
-      config.optimization.minimizer = config.optimization.minimizer.map(plugin => {
-        if (plugin.constructor.name === 'TerserPlugin') {
-          plugin.options.terserOptions = {
-            ...plugin.options.terserOptions,
-            // Prevent Terser from parsing the worker files
-            mangle: false,
-            compress: false,
-            parse: {
-              ecma: 8
-            },
-            module: true,
-          };
-        }
-        return plugin;
-      });
-    }
-
-    return config;
-  },
+// // @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ output:"export"
 };
+ 
+module.exports = nextConfig;
